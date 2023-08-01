2868af6c6f945aee482d04d46bd7da0c95c3852a
jerrystsai <j@jerrytsai.com>
2023-07-31 21:51:31 -0700

    Added User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+const { User } = require('../../db/models');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session isnot valid, set req.user to null
+router.use(restoreUser);
+
+
 
 // Add a XSRF-TOKEN cookie
 router.get("/csrf/restore", (req, res) => {
@@ -8,6 +17,32 @@ router.get("/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+//
+// Test auth middlewares
+//
+
+// Test set token cookie
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+
+// Test restore user
+// router.get('/restore-user', (req, res) => {
+//   return res.json(req.user);
+// });
+
+// Test require auth
+// router.get('/require-auth', requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
 
 // Test router
 router.post('/test', function(req, res) {
