ff4ac688a9d03f9e0a43faa55f7d66fb9092cc45
jerrystsai <j@jerrytsai.com>
2023-08-22 13:23:17 -0700

    See if this change gets rid of the stack trace in production, try 2

--- backend/app.js
+++ backend/app.js
@@ -68,7 +68,7 @@ app.use((err, _req, _res, next) => {
     }
     err.title = 'Validation error';
     err.errors = errors;
-    err.stack: isProduction ? null : err.stack
+    err.stack = isProduction ? null : err.stack;
   }
   next(err);
 });
