f302d1385360cd48bfdf7467f04228d22a5246b7
jerrystsai <j@jerrytsai.com>
2023-08-22 13:36:34 -0700

    See if this change gets rid of the stack trace in production, try 3

--- backend/app.js
+++ backend/app.js
@@ -77,12 +77,16 @@ app.use((err, _req, _res, next) => {
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
-  res.json({
-    // title: err.title || 'Server Error',
-    message: err.message,
-    errors: err.errors,
-    stack: isProduction ? null : err.stack
-  });
+
+  const errorMessageBody =
+    {
+      // title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+    };
+  if (!isProduction) errorMessageBody.stack = err.stack;
+
+  res.json(errorMessageBody);
 });
 
 // Exports
