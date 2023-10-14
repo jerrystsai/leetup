cd85cd451ceed98407548de52396d3f42e3000c0
jerrystsai <j@jerrytsai.com>
2023-10-14 12:55:48 -0700

    Added signup form

---
+++ frontend/src/components/SignupFormPage/index.js
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { userSignupThunk } from '../../store/session';
+import './SignupForm.css';
+
+const SignupFormPage = () => {
+
+  const dispatch = useDispatch();
+  const sessionUser = useSelector( state => state.session.user );
+
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  if (sessionUser) return <Redirect to='/' />;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      setErrors({});
+      const signupInfo = {email, username, firstName, lastName, password};
+      return dispatch(userSignupThunk(signupInfo)).catch(
+        async (res) => {
+          const data = await res.json();
+          if (data && data.errors) setErrors(data.errors)
+        }
+      );
+    }
+
+    return setErrors({
+      confirmPassword: "Confirm Password field must be the same as the Password field"
+    })
+  };
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        {errors.email && <div className="errors">{errors.email}</div>}
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        {errors.username && <div className="errors">{errors.username}</div>}
+        <label>
+          First name:
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        {errors.firstName && <div className="errors">{errors.firstName}</div>}
+        <label>
+          Last name:
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        {errors.lastName && <div className="errors">{errors.lastName}</div>}
+        <label>
+          Password:
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {errors.password && <div className="errors">{errors.password}</div>}
+        <label>
+          Confirm password:
+          <input
+            type="text"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+        {errors.confirmPassword && <div className="errors">{errors.confirmPassword}</div>}
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default SignupFormPage;
