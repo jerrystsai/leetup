f0b3c20b9f1e36f4f899ac84e58be8d2af1f9c55
jerrystsai <j@jerrytsai.com>
2023-10-16 17:34:39 -0700

    Added nice functionality to Profile button

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,24 +1,44 @@
-// import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import { logoutSessionThunk } from '../../store/session';
 
 const ProfileButton = ( {user} ) => {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  }
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(logoutSessionThunk());
   }
 
-  // const ulClassName = "profile-dropdown";
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+  useEffect( () => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
 
   return (
     <>
-      <h4>HERE HERE</h4>
-      <button style={ {color: "orange", fontSize: "50px"} }>
+      <button onClick={openMenu} style={ {color: "orange", fontSize: "50px"} }>
         <i className="fa-solid fa-address-card"></i>
       </button>
-      <ul className="profile-dropdown">
+      <ul className={ulClassName} ref={ulRef}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
